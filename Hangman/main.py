2ef98eba3160497b4b78f068a5e2d30700829c5b
Mariana Furriel <marianafurrielss@gmail.com>
2022-07-30 00:29:06 -0300

    Loop adicionado

--- Hangman/main.py
+++ Hangman/main.py
@@ -4,15 +4,17 @@ from menu import menu
 
 
 def main():
-    option = menu(1)
-
-    while((option != '1') and (option != '2') and (option != '3')):
+    option = 0
+    while(option != '3'):
         option = menu(1)
 
-    if option == '1':
-        jogar()
-    elif option == '2':
-        add_palavra()
+        while((option != '1') and (option != '2') and (option != '3')):
+            option = menu(1)
+
+        if option == '1':
+            jogar()
+        elif option == '2':
+            add_palavra()
 
 
 if __name__ == "__main__":
--- Hangman/sorteia_palavra.py
+++ Hangman/sorteia_palavra.py
@@ -3,5 +3,5 @@ from sorteia_elemento import sorteia_elemento
 
 
 def sorteia_palavra():
-    arquivo = ler_arquivo("palavras.txt")
+    arquivo = ler_arquivo()
     return sorteia_elemento(arquivo)[:-1]
