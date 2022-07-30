9cc582424e6eb6b2bd8f3da00c07001837d2d190
Mariana Furriel <marianafurrielss@gmail.com>
2022-07-30 15:04:29 -0300

    continuei janela de adicionar palavras

--- tela_add_palavra.py
+++ tela_add_palavra.py
@@ -4,5 +4,6 @@ from janela_def import *
 
 def tela_add_palavra():
     janela.title("Adicionar palavra")
-    texto_orientacao = Label(janela, text="Bem vindo! O que deseja fazer?")
+    texto_orientacao = Label(janela, text="Insira a palavra")
+    palavra = Text(janela)
     texto_orientacao.grid(column=0,row=0)    
