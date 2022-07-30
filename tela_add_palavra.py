7ba4c41911039dde91bbf2a6c3e48efab9af7247
Mariana Furriel <marianafurrielss@gmail.com>
2022-07-30 14:33:33 -0300

    Tela de adicionar palavra iniciada

---
+++ tela_add_palavra.py
@@ -0,0 +1,8 @@
+from add_palavra import add_palavra
+from tkinter import *
+from janela_def import *
+
+def tela_add_palavra():
+    janela.title("Adicionar palavra")
+    texto_orientacao = Label(janela, text="Bem vindo! O que deseja fazer?")
+    texto_orientacao.grid(column=0,row=0)    
